bf701566ac3684622a759a6c0ee313f1fb6316b4
noxim <aaro.peramaa@gmail.com>
2018-12-09 03:15:03 +0200

    [misc] checkpoint before huge refactor

--- target-switch/src/lib.rs
+++ target-switch/src/lib.rs
@@ -4,6 +4,8 @@
 #![allow(non_snake_case)]
 #![feature(core_intrinsics)]
 
+use core::mem::uninitialized;
+
 extern crate panic_abort;
 // extern crate kea;
 // extern crate game;
@@ -14,37 +16,36 @@ pub const fn null<T>() -> *mut T { 0 as *mut T }
 
 #[no_mangle]
 pub extern "C" fn main() {
-  unsafe {
-    consoleInit(null());
+    unsafe {
+        consoleInit(null());
 
-    let mut k_held_old = HidControllerKeys(0);
-    
-    printf("\x1b[1;1HKEA Engine on switch. Press PLUS to exit\n".as_ptr() as *const i8);
-    printf("\x1b[2;1H\n".as_ptr() as *const i8);
+        printf(
+            "\x1b[1;1HKEA Engine on switch. Press PLUS to exit\n".as_ptr()
+                as *const i8,
+        );
 
-    while appletMainLoop() {
-      hidScanInput();
-      let k_held = HidControllerKeys(hidKeysHeld(HidControllerID::CONTROLLER_P1_AUTO) as u32);
+        while appletMainLoop() {
+            hidScanInput();
+            let k_held = HidControllerKeys(hidKeysHeld(
+                HidControllerID::CONTROLLER_P1_AUTO,
+            ) as u32);
 
-      if k_held == HidControllerKeys::KEY_PLUS {
-        break;
-      }
+            if k_held == HidControllerKeys::KEY_PLUS {
+                break;
+            }
 
-      if k_held != k_held_old {
-        consoleClear();
+            consoleClear();
 
-        printf("\x1b[1;1HKEA Engine on switch. Press PLUS to exit\n".as_ptr() as *const i8);
-        printf("\x1b[2;1H\n".as_ptr() as *const i8);
-        printf("\x1b[3;1Hkeycode: %x\n".as_ptr() as *const i8, k_held);
-      }
+            printf(
+                "\x1b[1;1HKEA Engine on switch. Press PLUS to exit\n".as_ptr()
+                    as *const i8,
+            );
 
-      k_held_old = k_held;
+            consoleUpdate(null());
+        }
 
-      consoleUpdate(null());
+        consoleExit(null());
     }
-
-    consoleExit(null());
-  }
 }
 
 pub mod lang_items;
