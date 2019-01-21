542b637c9ca2d1c17bc3432094f2ee8e36b96ce1
noxim <aaro.peramaa@gmail.com>
2019-01-21 09:13:16 +0200

    [dev/drd] support android icons

--- target-android/src/main.rs
+++ target-android/src/main.rs
@@ -1,6 +1,7 @@
 extern crate kea;
 extern crate game;
 extern crate kea_dev;
+extern crate android_glue;
 
 struct Api;
 impl kea::PlatformApi for Api {
@@ -9,6 +10,23 @@ impl kea::PlatformApi for Api {
     }
 }
 
+struct AndroidHandler {}
+
+impl android_glue::SyncEventHandler for AndroidHandler {
+    fn handle(&mut self, event: &android_glue::Event) {
+        println!("{:#?}", event);
+        match event {
+            android_glue::Event::LostFocus => {
+                println!("FOCUS LOST: Save game state");
+                // TODO: ask game for its state and save it, and set reload flag
+                std::process::exit(0);
+            },
+            _ => (),
+        }
+    }
+}
+
 fn main() {  
+    android_glue::add_sync_event_handler(Box::new(AndroidHandler {}));
     kea::run(Api, kea_dev::Renderer::new(), &game::game);
 }
