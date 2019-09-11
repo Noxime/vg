62fe2154c0de1d3bac29ad52e962286f48289571
noxim <aaro.peramaa@gmail.com>
2019-09-11 17:25:33 +0300

    [misc] update game folder to new kea

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -1,31 +1,25 @@
 use kea::renderer::{Surface, Target};
-use audio::Clip;
+// use audio::Clip;
 use kea::*;
 
 const ASSETS: assets::Assets = asset_pack!("assets.keapack");
 
-pub fn game<P, R, I, A>(mut api: EngineApi<P, R, I, A>)
-where
-    P: PlatformApi,
-    R: renderer::Renderer,
-    I: input::Input,
-    A: audio::Audio,
-{
-    api.platform.print(&format!("assets.pack contains {} bytes of data", ASSETS.size()));
+pub fn run<A: Api>(mut api: A) {
+    println!("assets.pack contains {} bytes of data", ASSETS.size());
 
-    let mut clip = api.audio.from_vorbis(ASSETS.assets("audio").unwrap().binary("audio.ogg").unwrap());
-    clip.play();
+    // let mut clip = api.audio().from_vorbis(ASSETS.assets("audio").unwrap().binary("audio.ogg").unwrap());
+    // clip.play();
 
 
-    while !api.platform.exit() {
+    while !api.exit() {
         api.poll();
-
-        if api.input.controller(&api.input.default().unwrap()).unwrap().start.active() {
-            clip.stop();
-            clip.play();
+        let id = api.input().default().unwrap();
+        if api.input().controller(&id).unwrap().start.active() {
+            // clip.stop();
+            // clip.play();
         }
 
-        api.renderer.surface().set(&[0.65, 0.87, 0.91, 1.0]);
-        api.renderer.surface().present(true);
+        api.renderer().surface().set(&[0.65, 0.87, 0.91, 1.0]);
+        api.renderer().surface().present(true);
     }
 }
