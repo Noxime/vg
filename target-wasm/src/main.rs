f768b80cd4d7afa91475ca27ec6e661b136c0ace
noxim <aaro.peramaa@gmail.com>
2019-10-21 01:16:10 +0300

    [dev/web] start with stdweb backend

--- target-wasm/src/main.rs
+++ target-wasm/src/main.rs
@@ -1,4 +1,9 @@
-mod emscripten;
+#![feature(set_stdio)]
+
+use webgl_stdweb as webgl;
+use stdweb::{console, js};
+use stdweb::web::{self, html_element::CanvasElement, IParentNode, INode};
+use stdweb::unstable::TryInto;
 
 // The default allocator is quite big so this will make release binaries
 // smaller as size is a proper issue on the web
@@ -6,7 +11,7 @@ mod emscripten;
 #[cfg(not(debug_assertions))]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-async fn game(mut renderer: kea_sdl2::Renderer) {
+async fn game(mut api: impl kea::Renderer) {
     use kea::renderer::{Renderer, Surface, Target};
     println!("async loop start");
     loop {
@@ -19,15 +24,33 @@ async fn game(mut renderer: kea_sdl2::Renderer) {
 
 pub fn main() {
     // console_error_panic_hook::set_once();
-    println!("Running Kea engine on WASM");
+    console!(log, "Kea start");
 
-    let kea = kea_sdl2::Renderer::new();
+    let document = web::document();
+    let element = document.create_element("canvas").unwrap();
+    let canvas: CanvasElement = element.try_into().unwrap();
+    let body = document.body().unwrap();
+    body.append_child(&canvas);
+    canvas.set_width(800);
+    canvas.set_height(600);
+
+    console!(log, "Canvas created");
+
+    use webgl::WebGLRenderingContext as Ctx;
+    let ctx: webgl::WebGLRenderingContext = canvas.get_context().unwrap();
+
+    console!(log, "WebGL Error: ", ctx.get_error());
+
+    ctx.clear_color(1.0, 0.7, 1.0, 1.0);
+    ctx.clear(Ctx::COLOR_BUFFER_BIT);
+
+    console!(log, "Context: ", ctx.get_context_attributes());
+
+    // let kea = kea_sdl2::Renderer::new();
 
     use futures::future::{FutureExt, TryFutureExt};
     use futures::task::LocalSpawn;
     let mut executor = futures::executor::LocalPool::new();
-
-    async fn foo() {}
 
     executor
         .spawner()
@@ -36,10 +59,19 @@ pub fn main() {
 
     let main_loop = || {
         println!("Loop");
-        executor.run_until_stalled();
     };
 
-    println!("Starting main loop");
-    emscripten::emscripten::set_main_loop_callback(main_loop);
+    fn draw(ctx: Ctx, mut executor: futures::executor::LocalPool, x: f32) {
+        console!(log, "updating");
+        executor.run_until_stalled();
+
+        ctx.clear_color(x.sin() * 0.5 + 0.5, 0.5, 0.5, 1.0);
+        ctx.clear(Ctx::COLOR_BUFFER_BIT);
+
+        web::window().request_animation_frame(move |_| draw(ctx, executor, x + 1.0/60.0));
+    }
+
+    draw(ctx, executor, 0.0);
+
     println!("Engine exit (the bad way? oh god)");
 }
