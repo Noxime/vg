39ed333ba97d8ddaaffaa9e49dbdee5daf2770ce
noxim <aaro.peramaa@gmail.com>
2019-10-21 00:07:01 +0300

    [dev/web] move to futures-rs executor

--- target-wasm/src/main.rs
+++ target-wasm/src/main.rs
@@ -7,14 +7,13 @@ mod emscripten;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 async fn game(mut renderer: kea_sdl2::Renderer) {
-    use kea::renderer::{Surface, Target, Renderer};
+    use kea::renderer::{Renderer, Surface, Target};
     println!("async loop start");
     loop {
-        
         // renderer.poll();
         println!("Loop");
         // renderer.surface().set(&[0.65, 0.87, 0.91, 1.0]);
-        renderer.surface().present(true).await;
+        // renderer.surface().present(true).await;
     }
 }
 
@@ -22,39 +21,22 @@ pub fn main() {
     // console_error_panic_hook::set_once();
     println!("Running Kea engine on WASM");
 
-    use std::future::Future;
-    use std::pin::Pin;
-    use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-
     let kea = kea_sdl2::Renderer::new();
 
-    let fut = &mut game(kea);
-    let mut fut = unsafe { Pin::new_unchecked(fut) };
+    use futures::future::{FutureExt, TryFutureExt};
+    use futures::task::LocalSpawn;
+    let mut executor = futures::executor::LocalPool::new();
 
-    fn wake(_: *const ())     { println!("wake") }
-    fn wake_ref(_: *const ()) { println!("wake_ref") }
-    fn drop(_: *const ())     { println!("drop") }
-    fn clone(ptr: *const ()) -> RawWaker {
-        println!("clone");
-        RawWaker::new(
-            ptr,
-            &RawWakerVTable::new(clone, wake, wake_ref, drop),
-        )
-    }
+    async fn foo() {}
 
-    let waker = unsafe { Waker::from_raw(clone(&() as *const ())) };
-    let mut context = Context::from_waker(&waker);
+    executor
+        .spawner()
+        .spawn_local_obj(Box::new(async {}).into())
+        .expect("Failed to spawn");
 
-    let main_loop = move || {
-        // println!("Polling");
-        match fut.as_mut().poll(&mut context) {
-            Poll::Pending => println!("Expected"),
-            Poll::Ready(_) => {
-                println!("Game returned");
-                unsafe { emscripten::emscripten::emscripten_cancel_main_loop() }
-                println!("Exiting");
-            },
-        }
+    let main_loop = || {
+        println!("Loop");
+        executor.run_until_stalled();
     };
 
     println!("Starting main loop");
